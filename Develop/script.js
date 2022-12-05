ff0fca3d9530784fb2acbb56fd39af02e5d1ba62
isthatsilva <111096274+isthatsilva@users.noreply.github.com>
2022-12-05 15:05:34 -0500

    made each hour id equal to what will be set for local storage and add background color for each hour block

--- Develop/script.js
+++ Develop/script.js
@@ -13,9 +13,49 @@ var fifteen = $('#3pm');
 var sixteen = $('#4pm');
 var seventeen = $('#5pm');
 
+var userInput;
+var hourSpan;
+
 var hour = moment().hours();
 console.log(hour);
 
 var now = moment();
 $('#currentDay').text(now.format('dddd') + ' , ' + now.format('MMMM DD YYYY'));
 
+function createPage() {
+    $('#9am').val(JSON.parse(localStorage.getItem('09:00 am')));
+    $('#10am').val(JSON.parse(localStorage.getItem('10:00 am')));
+    $('#11am').val(JSON.parse(localStorage.getItem('11:00 am')));
+    $('#12pm').val(JSON.parse(localStorage.getItem('12:00 pm')));
+    $('#1pm').val(JSON.parse(localStorage.getItem('01:00 pm')));
+    $('#2pm').val(JSON.parse(localStorage.getItem('02:00 pm')));
+    $('#3pm').val(JSON.parse(localStorage.getItem('03:00 pm')));
+    $('#4pm').val(JSON.parse(localStorage.getItem('04:00 pm')));
+    $('#5pm').val(JSON.parse(localStorage.getItem('05:00 pm')));
+}
+
+function background() {
+    $('.form-control').each(function () {
+        const timeTest = parseInt($(this).attr('id'));
+        hour = parseInt(hour);
+        // console.log(timeTest);
+        
+        if (hour > timeTest) {
+            $(this).addClass('past');
+        } else if (hour < timeTest) {
+            $(this).addClass('future');
+        } else {
+            $(this).addClass('present');
+        }
+    });
+}
+
+$(document).ready(function () {
+    createPage()
+    background()
+
+    $('.savebtn').on('click', function () {
+        userInput = $(this).siblings('.form-control').val().trim();
+        console.log(userInput);
+    })
+})
